f30a74ebc9aeb3166ff7a2718bed2c93b71a38d3
s1f10210254 <s1f10210248@iniad.org>
2023-11-15 10:31:50 +0900

    login機能実装

--- client/src/pages/login_test/index.page.tsx
+++ client/src/pages/login_test/index.page.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import type { User } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
 import { registerWithEmail } from 'src/utils/login';
-// import { registerWithEmail } // ここで先ほど作成した登録関数をインポート
 
 const RegistrationForm: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return unsubscribe;
+  }, []);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -25,23 +36,36 @@ const RegistrationForm: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="email">メールアドレス:</label>
-        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="password">パスワード:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button type="submit">登録</button>
-      {message && <p>{message}</p>}
-    </form>
+    <div>
+      {user ? (
+        <div>
+          <p>ログイン中:{user.email}</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">メールアドレス:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">パスワード:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">登録</button>
+          {message && <p>{message}</p>}
+        </form>
+      )}
+    </div>
   );
 };
 
